ccb3b38508acf7791ad14046b6c57d3c20165de7
Blaine Price <blaine@ario.com>
2021-12-05 00:09:36 -0500

    tests work but dang

--- aoc2021-4/src/main.rs
+++ aoc2021-4/src/main.rs
@@ -2,11 +2,13 @@ use std::fs;
 use std::vec;
 
 fn main() {
-    println!("Hello, world!");
     let input: String = fs::read_to_string("input").expect("couldn't read the file");
 
     let part_one = part_one(&input);
     dbg!(part_one);
+
+    let part_two = part_two(&input);
+    dbg!(part_two);
 }
 
 #[derive(Debug)]
@@ -62,6 +64,7 @@ impl Board {
                     }
                 })
                 .collect();
+
             if marked_cells.len() == self.columns {
                 return true;
             }
@@ -132,12 +135,43 @@ fn part_one(input: &String) -> i64 {
         for board in &boards {
             if board.is_winner(&moves[0..index]) {
                 let score = board.get_score(&moves[0..index]);
-                let last_number = moves[index - 1];
-                return score * last_number;
+                let last_number = &moves[0..index].last().unwrap();
+                return score * *last_number;
             }
         }
     }
-    0
+    -1
+}
+
+fn part_two(input: &String) -> i64 {
+    let inputs = split_input_by_blankline(input);
+    // Get the moves
+    let moves = get_moves(&inputs[0]);
+
+    let mut boards: Vec<Board> = vec![];
+    for board_string in &inputs[1..] {
+        boards.push(Board::new(5, 5, board_string));
+    }
+
+    for index in 0..moves.len() {
+        let mut to_remove: Vec<usize> = vec![];
+        for (board_index, board) in boards.iter().enumerate() {
+            // Otherwise, check if Increment winners
+            if board.is_winner(&moves[0..index]) {
+                if boards.len() == 1 {
+                    let score = board.get_score(&moves[0..index]);
+                    let last_number = &moves[0..index].last().unwrap();
+                    return score * *last_number;
+                }
+
+                to_remove.push(board_index);
+            }
+        }
+        for index in to_remove {
+            boards.remove(index);
+        }
+    }
+    -1
 }
 
 #[cfg(test)]
@@ -292,5 +326,31 @@ mod test {
             .to_string();
         let output = part_one(&input);
         assert_eq!(output, 4512);
+    }
+
+    #[test]
+    fn it_finds_the_last_winning_board() {
+        let input = r#"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+8  2 23  4 24
+21  9 14 16  7
+6 10  3 18  5
+1 12 20 15 19
+
+3 15  0  2 22
+9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+2  0 12  3  7"#
+            .to_string();
+        let output = part_two(&input);
+        assert_eq!(output, 1924);
     }
 }

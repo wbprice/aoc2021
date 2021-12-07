d6aa75e47662adeaf5306268a736ceb808ef5745
Blaine Price <blaine@ario.com>
2021-12-07 12:42:05 -0500

    cleanup

--- aoc2021-7/src/main.rs
+++ aoc2021-7/src/main.rs
@@ -12,39 +12,35 @@ fn main() {
         })
         .collect();
 
-    let dest = chart_cheapest_alignment_destination(&initial_state);
-    let cost = calculate_alignment_fuel_cost(&initial_state, dest);
+    let cost = get_cheapest_destination_cost(&initial_state);
     dbg!(cost);
 
-    let dest = chart_cheapest_alignment_destination_v2(&initial_state);
-    let cost = calculate_alignment_fuel_cost_v2(&initial_state, dest);
+    let cost = get_cheapest_destination_cost_v2(&initial_state);
     dbg!(cost);
 }
 
-fn chart_cheapest_alignment_destination(input: &[i32]) -> i32 {
+fn get_cheapest_destination_cost(input: &[i32]) -> Option<i32> {
     // Calcuate the fuel cost for each destination
     let mut fuel_costs: HashMap<i32, i32> = HashMap::new();
     for destination in input {
         if fuel_costs.get(destination).is_none() {
-            let cost = calculate_alignment_fuel_cost(input, *destination);
+            let cost = calculate_destination_fuel_cost(input, *destination);
             fuel_costs.insert(cost, *destination);
         }
     }
 
-    // Return the position that is cheapest for everyone to travel to
-    let lowest_cost = fuel_costs.clone().into_keys().min().unwrap();
-    let dest = fuel_costs.get(&lowest_cost).unwrap();
-    *dest
+    // Return the cost of the cheapest destination
+    fuel_costs.into_keys().min()
 }
 
-fn calculate_alignment_fuel_cost(input: &[i32], destination: i32) -> i32 {
+fn calculate_destination_fuel_cost(input: &[i32], destination: i32) -> i32 {
     input
         .iter()
         .map(|&value| i32::abs(value - destination))
         .sum::<i32>()
 }
 
-fn chart_cheapest_alignment_destination_v2(input: &[i32]) -> i32 {
+fn get_cheapest_destination_cost_v2(input: &[i32]) -> Option<i32> {
     let min = input.iter().min().expect("Couldn't find the minimum");
     let max = input.iter().max().expect("Couldn't find the maximum");
 
@@ -55,25 +51,22 @@ fn chart_cheapest_alignment_destination_v2(input: &[i32]) -> i32 {
         // Calculate how expensive it would be to move everyone there
         if fuel_costs.get(&destination).is_none() {
             fuel_costs.insert(
-                calculate_alignment_fuel_cost_v2(input, destination),
+                calculate_destination_fuel_cost_v2(input, destination),
                 destination,
             );
         }
     }
 
-    // Return the position that is cheapest for everyone to travel to
-    let lowest_cost = fuel_costs.clone().into_keys().min().unwrap();
-    let dest = fuel_costs.get(&lowest_cost).unwrap();
-    *dest
+    // Return the cost of the cheapest destination
+    fuel_costs.clone().into_keys().min()
 }
 
-fn calculate_alignment_fuel_cost_v2(input: &[i32], destination: i32) -> i32 {
+fn calculate_destination_fuel_cost_v2(input: &[i32], destination: i32) -> i32 {
     input
         .iter()
         .map(|&value| {
             let steps = i32::abs(value - destination);
-            let cost = (0..steps + 1).sum::<i32>();
-            cost
+            (0..steps + 1).sum::<i32>()
         })
         .sum::<i32>()
 }
@@ -88,42 +81,42 @@ mod tests {
             .map(|v| v.parse().unwrap())
             .collect();
 
-        let output = calculate_alignment_fuel_cost(&input, 2);
+        let output = calculate_destination_fuel_cost(&input, 2);
         assert_eq!(output, 37);
     }
 
     #[test]
-    fn it_finds_the_cheapest_alignment_destination() {
+    fn it_finds_the_cheapest_destination_cost() {
         let input: Vec<i32> = "16,1,2,0,4,2,7,1,2,14"
             .split(',')
             .map(|v| v.parse().unwrap())
             .collect();
 
-        let output = chart_cheapest_alignment_destination(&input);
-        assert_eq!(output, 2);
+        let output = get_cheapest_destination_cost(&input);
+        assert_eq!(output, Some(37));
     }
 
     #[test]
-    fn it_finds_the_cheapest_alignment_cost_v2() {
+    fn it_calculates_the_fuel_cost_to_align_crabs_v2() {
         let input: Vec<i32> = "16,1,2,0,4,2,7,1,2,14"
             .split(',')
             .map(|v| v.parse().unwrap())
             .collect();
 
-        let output = calculate_alignment_fuel_cost_v2(&input, 5);
+        let output = calculate_destination_fuel_cost_v2(&input, 5);
         assert_eq!(output, 168);
-        let output = calculate_alignment_fuel_cost_v2(&input, 2);
+        let output = calculate_destination_fuel_cost_v2(&input, 2);
         assert_eq!(output, 206);
     }
 
     #[test]
-    fn it_finds_the_cheapest_alignment_destination_v2() {
+    fn it_finds_the_cheapest_destination_cost_v2() {
         let input: Vec<i32> = "16,1,2,0,4,2,7,1,2,14"
             .split(',')
             .map(|v| v.parse().unwrap())
             .collect();
 
-        let output = chart_cheapest_alignment_destination_v2(&input);
-        assert_eq!(output, 5);
+        let output = get_cheapest_destination_cost_v2(&input);
+        assert_eq!(output, Some(168));
     }
 }

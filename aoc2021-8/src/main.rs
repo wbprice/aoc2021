a13f2e6f133532e004148e6f7a6036a123ceb79a
Blaine Price <blaine@ario.com>
2021-12-08 23:00:08 -0500

    potato

--- aoc2021-8/src/main.rs
+++ aoc2021-8/src/main.rs
@@ -97,25 +97,164 @@ fn signal_pattern_matcher(input: String, map: HashMap<String, char>) -> u64 {
     string.parse().unwrap()
 }
 
-fn input_descrambler(input: &[String]) -> HashMap<String, char> {
-    let mut output: HashMap<String, char> = HashMap::new();
+fn fits_a_four(input: &String, map: &HashMap<u8, String>) -> bool {
+    let four = map.get(&4).unwrap();
+    let mut i = 0;
+    for c in four.chars() {
+        for v in input.chars() {
+            if c == v {
+                i += 1;
+            }
+        }
+    }
+
+    i == 4
+}
+
+fn fits_a_seven(input: &String, map: &HashMap<u8, String>) -> bool {
+    let seven = map.get(&7).unwrap();
+
+    let mut i = 0;
+    for c in seven.chars() {
+        for v in input.chars() {
+            if c == v {
+                i += 1;
+            }
+        }
+    }
+
+    i == seven.len()
+}
+
+fn fits_a_one(input: &String, map: &HashMap<u8, String>) -> bool {
+    let one = map.get(&1).unwrap();
+
+    let mut i = 0;
+    for c in one.chars() {
+        for v in input.chars() {
+            if c == v {
+                i += 1;
+            }
+        }
+    }
+
+    i == one.len()
+}
+
+fn has_lower_right_segment(input: &String, map: &HashMap<Segment, String>) -> bool {
+    let lower_right = map.get(&Segment::LowerRight).unwrap();
+
+    for v in input.chars() {
+        if v.to_string() == *lower_right {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn get_common_segments(inputA: &String, inputB: &String) -> Vec<String> {
+    let mut output: Vec<String> = vec![];
+
+    for a in inputA.chars() {
+        for b in inputB.chars() {
+            if a == b {
+                output.push(a.to_string());
+            }
+        }
+    }
+
+    output
+}
+
+#[derive(Eq, PartialEq, Hash)]
+enum Segment {
+    Top,
+    Middle,
+    UpperRight,
+    LowerRight,
+}
+
+fn get_input_descrambler(input: &[String]) -> HashMap<u8, String> {
+    let mut output: HashMap<u8, String> = HashMap::new();
+    let mut segments: HashMap<Segment, String> = HashMap::new();
+
+    // Freebies
     // Figure out where 1, 4, 7, and 8 are:
     for value in input {
         match value.len() {
             2 => {
-                output.insert(value.to_string(), '1');
+                output.insert(1, value.to_string());
             }
             4 => {
-                output.insert(value.to_string(), '4');
+                output.insert(4, value.to_string());
             }
             3 => {
-                output.insert(value.to_string(), '7');
+                output.insert(7, value.to_string());
             }
             7 => {
-                output.insert(value.to_string(), '8');
+                output.insert(8, value.to_string());
             }
             _ => {
                 // noop
+            }
+        }
+    }
+
+    // Derived from freebies
+    // Figure out the 6 segment numbers
+    let mut six_segments_found = 0;
+    while six_segments_found < 3 {
+        for value in input {
+            match value.len() {
+                6 => {
+                    if fits_a_four(value, &output) {
+                        output.insert(9, value.to_string());
+                        six_segments_found += 1;
+                    } else if !fits_a_four(value, &output) && fits_a_one(value, &output) {
+                        output.insert(0, value.to_string());
+                        six_segments_found += 1;
+                    } else {
+                        output.insert(6, value.to_string());
+                        let one = output.get(&1).unwrap();
+                        let common = get_common_segments(value, one);
+                        segments.insert(Segment::LowerRight, common.get(0).unwrap().to_string());
+                        six_segments_found += 1;
+                    }
+                }
+                5 => {
+                    if fits_a_seven(value, &output) {
+                        output.insert(3, value.to_string());
+                        six_segments_found += 1;
+                    }
+                }
+                _ => {
+                    // noop
+                }
+            }
+        }
+    }
+
+    // Figure out the five segment numbers
+    let mut five_segments_found = 0;
+    while five_segments_found < 3 {
+        for value in input {
+            match value.len() {
+                5 => {
+                    if fits_a_seven(value, &output) {
+                        output.insert(3, value.to_string());
+                        five_segments_found += 1;
+                    } else if has_lower_right_segment(value, &segments) {
+                        output.insert(2, value.to_string());
+                        five_segments_found += 1;
+                    } else {
+                        output.insert(5, value.to_string());
+                        five_segments_found += 1;
+                    }
+                }
+                _ => {
+                    // noop
+                }
             }
         }
     }
@@ -176,10 +315,51 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
     }
 
     #[test]
-    fn it_reads_outputs_into_numbers() {
-        let input = "fdgacbe cefdb cefbgd gcbe".to_string();
-        let dictionary = get_pattern_matcher_map();
-        let output = signal_pattern_matcher(input, dictionary);
+    fn it_returns_true_if_a_four_would_fit() {
+        let input: Vec<String> = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb"
+            .to_string()
+            .split_whitespace()
+            .map(|value| value.to_string())
+            .collect();
+
+        let map = get_input_descrambler(&input);
+        let output = fits_a_four(&"cbdgef".to_string(), &map);
+        assert_eq!(output, true);
+
+        let output = fits_a_four(&"agebfd".to_string(), &map);
+        assert_eq!(output, false)
+    }
+
+    #[test]
+    fn it_returns_true_if_a_seven_would_fit() {
+        let input: Vec<String> = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb"
+            .to_string()
+            .split_whitespace()
+            .map(|value| value.to_string())
+            .collect();
+
+        let map = get_input_descrambler(&input);
+        let output = fits_a_seven(&"fecdb".to_string(), &map);
+        assert_eq!(output, true);
+
+        let output = fits_a_seven(&"fdcge".to_string(), &map);
+        assert_eq!(output, false)
+    }
+
+    #[test]
+    fn it_returns_true_if_a_one_would_fit() {
+        let input: Vec<String> = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb"
+            .to_string()
+            .split_whitespace()
+            .map(|value| value.to_string())
+            .collect();
+
+        let map = get_input_descrambler(&input);
+        let output = fits_a_one(&"agebfd".to_string(), &map);
+        assert_eq!(output, true);
+
+        let output = fits_a_one(&"fgaecd".to_string(), &map);
+        assert_eq!(output, false)
     }
 
     #[test]
@@ -190,7 +370,7 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
             .map(|value| value.to_string())
             .collect();
 
-        let output = input_descrambler(&input);
+        let output = get_input_descrambler(&input);
         dbg!(output);
     }
 }

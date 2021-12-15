6e256e9e5e0d8684ef37ebbdecd2ee823a766577
Blaine Price <blaine@ario.com>
2021-12-15 11:06:41 -0500

    not working any faster

--- aoc2021-14/src/main.rs
+++ aoc2021-14/src/main.rs
@@ -11,9 +11,9 @@ fn main() {
     let inputs = split_input_by_blankline(&input);
     let polymer = &inputs[0].to_string();
     let polymer_rules: Vec<String> = inputs[1].split("\n").map(|line| line.to_string()).collect();
-    let polymer_map = get_polymer_map(&polymer_rules);
+    let polymer_map = get_polymer_rules(&polymer_rules);
 
-    let output = model_polymerization(polymer, &polymer_map, 10);
+    let output = model_polymerization(polymer, 10, &polymer_map);
     let mut counts: Vec<u64> = output.into_values().collect();
     counts.sort_unstable();
     let highest = counts.last().unwrap();
@@ -21,7 +21,7 @@ fn main() {
     let difference = highest - lowest;
     dbg!(difference);
 
-    let output = model_polymerization(polymer, &polymer_map, 40);
+    let output = model_polymerization(polymer, 20, &polymer_map);
     let mut counts: Vec<u64> = output.into_values().collect();
     counts.sort_unstable();
     let highest = counts.last().unwrap();
@@ -37,7 +37,7 @@ fn split_input_by_blankline(input: &str) -> Vec<String> {
         .collect()
 }
 
-fn get_polymer_map(input: &[String]) -> PolymerRules {
+fn get_polymer_rules(input: &[String]) -> PolymerRules {
     let mut output: PolymerRules = HashMap::new();
     for line in input {
         let split: Vec<&str> = line.split(" -> ").collect();
@@ -61,117 +61,95 @@ fn count_polymers(polymer: &str) -> PolymerInventory {
     output
 }
 
-fn polymerize(polymer: Polymer, polymer_map: &PolymerRules) -> Polymer {
-    let mut output = "".to_string();
+fn polymerize(
+    polymer: Polymer,
+    steps: u64,
+    rules: &PolymerRules,
+    inventory: &mut PolymerInventory,
+) {
+    // Base case
+    if steps == 0 {
+        return;
+    }
 
+    // Recursive case
     let chars: Vec<char> = polymer.chars().collect();
     chars.clone().windows(2).for_each(|window| {
         let key = window
             .iter()
             .fold("".to_string(), |acc, x| acc + &x.to_string());
-        if let Some(value) = polymer_map.get(&key) {
-            output += &format!("{}{}", window[0], value,);
-        }
-    });
-
-    output += chars.last().unwrap().to_string().as_str();
-
-    output
-}
-
-fn agg_polymerize(
-    agg_polymer: &PolymerInventory,
-    polymer_rules: &PolymerRules,
-) -> PolymerInventory {
-    let mut output = PolymerInventory::new();
-
-    // Each polymer pair in `agg_polymer` creates two new polymer pairs in the output
-    // according to polymer rules
-    for polymer_pair in agg_polymer.clone().into_keys() {
-        if let Some(new_polymer) = polymer_rules.get(&polymer_pair) {
-            let polymer_pair_split: Vec<char> = polymer_pair.chars().collect();
-            let polymer_a = format!("{}{}", polymer_pair_split[0], new_polymer);
-            let polymer_b = format!("{}{}", new_polymer, polymer_pair_split[1]);
-
-            if let Some(count) = output.get_mut(&polymer_a) {
-                *count += 1;
+        if let Some(new_polymer) = rules.get(&key) {
+            // Add the new polymer to the inventory
+            if let Some(value) = inventory.get_mut(new_polymer) {
+                *value += 1;
             } else {
-                output.insert(polymer_a, 1);
+                inventory.insert(new_polymer.to_string(), 1);
             }
 
-            if let Some(count) = output.get_mut(&polymer_b) {
-                *count += 1;
-            } else {
-                output.insert(polymer_b, 1);
-            }
+            // Recurse with new polymers
+            polymerize(
+                format!("{}{}", window[0], new_polymer),
+                steps - 1,
+                rules,
+                inventory,
+            );
+            polymerize(
+                format!("{}{}", new_polymer, window[1]),
+                steps - 1,
+                rules,
+                inventory,
+            );
         }
-    }
-
-    output
+    })
 }
 
-fn model_polymerization(
-    polymer: &Polymer,
-    polymer_map: &PolymerRules,
-    steps: u64,
-) -> PolymerInventory {
-    let mut polymer = polymer.to_string();
-    for _i in 0..steps {
-        polymer = polymerize(polymer, &polymer_map);
-    }
-
-    count_polymers(&polymer)
-}
-
-fn agg_model_polymerization(
+fn polymerize_v2(
+    polymer: Polymer,
+    depth: u64,
+    rules: &PolymerRules,
     inventory: PolymerInventory,
-    polymer_rules: &PolymerRules,
-    steps: u64,
 ) -> PolymerInventory {
-    let mut inventory = inventory.clone();
-    for _i in 0..steps {
-        inventory = agg_polymerize(&inventory, polymer_rules);
+    // Base case
+    // Return the inventory when the maximum depth is reached
+    if depth == 0 {
+        return inventory;
     }
 
-    inventory
+    // Recursive case
+    // For each window in polymer, polymerize one layer lower
+    polymer
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(2)
+        .fold(inventory, |inventory, window| {
+            let polymer_pair = format!("{}{}", window[0], window[1]);
+            if let Some(new_polymer) = rules.clone().get(&polymer_pair) {
+                // Update inventory
+                let mut next_inventory = inventory.clone();
+                if let Some(value) = inventory.get(new_polymer) {
+                    next_inventory.insert(new_polymer.to_string(), value + 1);
+                } else {
+                    next_inventory.insert(new_polymer.to_string(), 1);
+                }
+
+                // Call polymerize on both new polymers
+                vec![
+                    format!("{}{}", window[0], new_polymer),
+                    format!("{}{}", new_polymer, window[1]),
+                ]
+                .iter()
+                .fold(next_inventory, |inventory, polymer| {
+                    polymerize_v2(polymer.to_string(), depth - 1, &rules, inventory)
+                })
+            } else {
+                inventory
+            }
+        })
 }
 
-fn agg_count_polymers(inventory: PolymerInventory) -> PolymerInventory {
-    let mut left_map = PolymerInventory::new();
-    let mut right_map = PolymerInventory::new();
-    let mut output = PolymerInventory::new();
-
-    for key in inventory.clone().into_keys() {
-        let chars: Vec<char> = key.clone().chars().collect();
-        let left_key = chars.get(0).unwrap().to_string();
-        let right_key = chars.get(1).unwrap().to_string();
-
-        if let Some(count) = inventory.get(&key) {
-            if let Some(left_count) = left_map.get_mut(&left_key) {
-                *left_count += count;
-            } else {
-                left_map.insert(left_key, *count);
-            }
-        }
-
-        if let Some(count) = inventory.get(&key) {
-            if let Some(right_count) = right_map.get_mut(&right_key) {
-                *right_count += count;
-            } else {
-                right_map.insert(right_key, *count);
-            }
-        }
-    }
-
-    for left_key in left_map.clone().into_keys() {
-        if let Some(&left_value) = left_map.get(&left_key) {
-            if let Some(&right_value) = right_map.get(&left_key) {
-                output.insert(left_key, left_value.max(right_value));
-            }
-        }
-    }
-
-    output
+fn model_polymerization(polymer: &Polymer, steps: u64, rules: &PolymerRules) -> PolymerInventory {
+    let polymer_inventory = count_polymers(&polymer);
+    polymerize_v2(polymer.to_string(), steps, &rules, polymer_inventory)
 }
 
 #[cfg(test)]
@@ -202,7 +180,7 @@ CN -> C"#;
         let inputs = split_input_by_blankline(INPUT);
         let polymer_rules: Vec<String> =
             inputs[1].split("\n").map(|line| line.to_string()).collect();
-        let output = get_polymer_map(&polymer_rules);
+        let output = get_polymer_rules(&polymer_rules);
         assert_eq!(output.into_keys().len(), 16);
     }
 
@@ -212,28 +190,11 @@ CN -> C"#;
         let polymer = &inputs[0].to_string();
         let polymer_rules: Vec<String> =
             inputs[1].split("\n").map(|line| line.to_string()).collect();
-        let polymer_map = get_polymer_map(&polymer_rules);
-
-        let polymer = polymer.to_string();
-        let polymer = polymerize(polymer, &polymer_map);
-        assert_eq!(polymer, "NCNBCHB");
-        let polymer = polymerize(polymer, &polymer_map);
-        assert_eq!(polymer, "NBCCNBBBCBHCB");
-        let polymer = polymerize(polymer, &polymer_map);
-        assert_eq!(polymer, "NBBBCNCCNBBNBNBBCHBHHBCHB");
-        let polymer = polymerize(polymer, &polymer_map);
-        assert_eq!(polymer, "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB");
-    }
-
-    #[test]
-    fn it_counts_polymers() {
-        let inputs = split_input_by_blankline(INPUT);
-        let polymer = &inputs[0];
-        let polymer_rules: Vec<String> =
-            inputs[1].split("\n").map(|line| line.to_string()).collect();
-        let polymer_map = get_polymer_map(&polymer_rules);
-
-        let output = model_polymerization(polymer, &polymer_map, 10);
+        
+        let polymer_rules = get_polymer_rules(&polymer_rules);
+        let polymer_inventory = count_polymers(polymer);
+        let output =
+            polymerize_v2(polymer.to_string(), 10, &polymer_rules, polymer_inventory);
 
         let mut counts: Vec<u64> = output.into_values().collect();
         counts.sort_unstable();
@@ -241,39 +202,5 @@ CN -> C"#;
         let lowest = counts.first().unwrap();
         let difference = highest - lowest;
         assert_eq!(difference, 1588);
-    }
-
-    #[test]
-    fn it_agg_polymerizes() {
-        let inputs = split_input_by_blankline(INPUT);
-        let polymer_rules: Vec<String> =
-            inputs[1].split("\n").map(|line| line.to_string()).collect();
-        let polymer_map = get_polymer_map(&polymer_rules);
-
-        // starting polymer is 'NNCB'
-        let agg_polymer: HashMap<String, u64> = HashMap::from([
-            ("NN".to_string(), 1),
-            ("NC".to_string(), 1),
-            ("CB".to_string(), 1),
-        ]);
-
-        //
-        // 0. NNCB
-        // - NCN NBC CHB
-        // 1. NC CN NB BC CH HB
-        // - NBC CCN BBB BBC CBH HCB
-        // 2. NB BC CC CN BB BB BB BC CB BH HC CB
-
-        let agg_polymer = agg_model_polymerization(agg_polymer, &polymer_map, 10);
-        // assert_eq!(agg_polymer.clone().into_keys().len(), 6);
-        // assert_eq!(agg_polymer["CH"], 1);
-        // assert_eq!(agg_polymer["NC"], 1);
-        // assert_eq!(agg_polymer["BC"], 1);
-        // assert_eq!(agg_polymer["HB"], 1);
-        // assert_eq!(agg_polymer["CN"], 1);
-        // assert_eq!(agg_polymer["NB"], 1);
-
-        let output = agg_count_polymers(agg_polymer);
-        dbg!(output);
     }
 }

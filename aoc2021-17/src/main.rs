77d8b00f9c0ecf8ef6908d6438cc65b1c00e4848
Blaine Price <blaine@ario.com>
2021-12-19 17:26:30 -0500

    part one

--- aoc2021-17/src/main.rs
+++ aoc2021-17/src/main.rs
@@ -2,16 +2,22 @@ use std::ops::RangeInclusive;
 
 fn main() {
     // target area: x=281..311, y=-74..-54
-    let apex = find_highest_successful_arc_height((281..=311, -74..=-54), 1000);
+    let apex = find_highest_successful_arc_height((281..=311, -74..=-54));
     dbg!(apex);
 }
 
-fn simulate_probe_arc(velocity: (i32, i32), max_steps: i32) -> Vec<(i32, i32)> {
+fn simulate_probe_arc(
+    velocity: (i32, i32),
+    goal: &(RangeInclusive<i32>, RangeInclusive<i32>),
+) -> Vec<(i32, i32)> {
     let mut positions = vec![];
     let mut position = (0, 0);
     let mut velocity = velocity;
-    for _step in 0..max_steps {
+    loop {
         let (p, v) = model_probe_arc(position, velocity);
+        if check_probe_out_of_bounds(p, goal) {
+            break;
+        }
         positions.push(p);
         position = p;
         velocity = v;
@@ -38,6 +44,16 @@ fn check_probe_was_in_goal(
     false
 }
 
+fn check_probe_out_of_bounds(
+    position: (i32, i32),
+    goal: &(RangeInclusive<i32>, RangeInclusive<i32>),
+) -> bool {
+    if goal.0.end() < &position.0 || goal.1.start() > &position.1 {
+        return true;
+    }
+    false
+}
+
 fn get_apex_of_arc(arc: &[(i32, i32)]) -> i32 {
     let mut highest = 0;
     for position in arc {
@@ -48,15 +64,12 @@ fn get_apex_of_arc(arc: &[(i32, i32)]) -> i32 {
     highest
 }
 
-fn find_highest_successful_arc_height(
-    goal: (RangeInclusive<i32>, RangeInclusive<i32>),
-    max_steps: i32,
-) -> i32 {
+fn find_highest_successful_arc_height(goal: (RangeInclusive<i32>, RangeInclusive<i32>)) -> i32 {
     // fuzz values between x 0 and 10 and y -10 and 10
     let mut heights = vec![];
-    for y in -1000..1000 {
+    for y in -0..1000 {
         for x in 0..1000 {
-            let arc = simulate_probe_arc((x, y), max_steps);
+            let arc = simulate_probe_arc((x, y), &goal);
             if check_probe_was_in_goal(&arc, &goal) {
                 let apex = get_apex_of_arc(&arc);
                 heights.push(apex);
@@ -73,26 +86,27 @@ mod tests {
 
     #[test]
     fn it_checks_if_a_probe_is_in_the_goal() {
-        let arc = simulate_probe_arc((7, 2), 7);
-        let result = check_probe_was_in_goal(&arc, &(20..=30, -10..=-5));
+        let goal = (20..=30, -10..=-5);
+        let arc = simulate_probe_arc((7, 2), &goal);
+        let result = check_probe_was_in_goal(&arc, &goal);
         assert_eq!(result, true);
 
-        let arc = simulate_probe_arc((6, 3), 9);
-        let result = check_probe_was_in_goal(&arc, &(20..=30, -10..=-5));
+        let arc = simulate_probe_arc((6, 3), &goal);
+        let result = check_probe_was_in_goal(&arc, &goal);
         assert_eq!(result, true);
 
-        let arc = simulate_probe_arc((9, 0), 4);
-        let result = check_probe_was_in_goal(&arc, &(20..=30, -10..=-5));
+        let arc = simulate_probe_arc((9, 0), &goal);
+        let result = check_probe_was_in_goal(&arc, &goal);
         assert_eq!(result, true);
 
-        let arc = simulate_probe_arc((17, -4), 2);
-        let result = check_probe_was_in_goal(&arc, &(20..=30, -10..=-5));
+        let arc = simulate_probe_arc((17, -4), &goal);
+        let result = check_probe_was_in_goal(&arc, &goal);
         assert_eq!(result, false);
     }
 
     #[test]
     fn it_finds_the_coolest_probe_shot() {
-        let apex = find_highest_successful_arc_height((20..=30, -10..=-5), 20);
+        let apex = find_highest_successful_arc_height((20..=30, -10..=-5));
         assert_eq!(apex, 45);
     }
 }
